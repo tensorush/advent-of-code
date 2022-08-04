ac1c2e99ff75988581d2524b40b8c5a671b6d71e
Georgy Trush <tensorush@gmail.com>
2022-08-04 19:36:41 +0300

    Update build.zig

--- 2016-Zig/build.zig
+++ 2016-Zig/build.zig
@@ -1,6 +1,6 @@
-const std = @import("std");
+const Builder = @import("std").build.Builder;
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const target = b.standardTargetOptions(.{});
 
@@ -17,25 +17,25 @@ pub fn build(b: *std.build.Builder) void {
         exe.setBuildMode(mode);
         exe.setTarget(target);
 
-        const install_exe = b.addInstallArtifact(exe);
+        const install_cmd = b.addInstallArtifact(exe);
 
-        const run_exe = exe.run();
-        run_exe.step.dependOn(&install_exe.step);
+        const run_cmd = exe.run();
+        run_cmd.step.dependOn(&install_cmd.step);
 
-        const test_exe = b.addTest(day_file);
-        test_exe.setTarget(target);
-        test_exe.setBuildMode(mode);
+        const test_cmd = b.addTest(day_file);
+        test_cmd.setTarget(target);
+        test_cmd.setBuildMode(mode);
 
         const run_step = b.step(b.fmt("run_{s}", .{str_day}), b.fmt("Run {s}", .{str_day}));
-        run_step.dependOn(&run_exe.step);
-        run_all_step.dependOn(&run_exe.step);
+        run_step.dependOn(&run_cmd.step);
+        run_all_step.dependOn(&run_cmd.step);
 
         const test_step = b.step(b.fmt("test_{s}", .{str_day}), b.fmt("Test {s}", .{str_day}));
-        test_step.dependOn(&test_exe.step);
-        test_all_step.dependOn(&test_exe.step);
+        test_step.dependOn(&test_cmd.step);
+        test_all_step.dependOn(&test_cmd.step);
 
         const install_step = b.step(b.fmt("install_{s}", .{str_day}), b.fmt("Install {s}", .{str_day}));
-        install_step.dependOn(&install_exe.step);
-        install_all_step.dependOn(&install_exe.step);
+        install_step.dependOn(&install_cmd.step);
+        install_all_step.dependOn(&install_cmd.step);
     }
 }
